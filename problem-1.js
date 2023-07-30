e8923187d48607c8a080e095913bb71e6b05c049
Aaban Nayeem <aabannayeem4358@gmail.com>
2023-07-31 00:01:53 +0600

    solve problem 1

--- problem-1.js
+++ problem-1.js
@@ -1,1 +1,34 @@
-//^ Array of fruit 
+//^ Given that the Array of fruit
+let fruits = ["Apple", "Banana", "Orange"];
+
+//* a) Find the index of 'Banana' and replace it with 'Mango'
+
+//todo: Resquirement - i
+
+let findBanana = fruits.indexOf("Banana");
+console.log(findBanana);
+
+//todo: Resquirement - ii
+
+// Website link: https://www.geeksforgeeks.org/how-to-replace-an-item-from-an-array-in-javascript/
+
+//? Method 1: Using Array Indexing
+/*
+let index = 1;
+let newValue = "Mango";
+fruits[index] = newValue;
+console.log(fruits);
+*/
+
+//? Method 2: Using the splice() Method
+
+fruits.splice(1, 1, "Mango");
+console.log(fruits);
+
+//? Method 3: Using array map() and filter() Methods
+
+//* b) Remove 'Orange' and add 'Watermelonq'.
+
+let lastFruit = fruits.pop();
+let addLastFruit = fruits.push("Watermelon");
+console.log(fruits);
