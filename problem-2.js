293da51fc3221d111482b2b08eb66ead7bb8f90e
Aaban Nayeem <aabannayeem4358@gmail.com>
2023-07-31 12:03:33 +0600

    solve problem 2

---
+++ problem-2.js
@@ -0,0 +1,15 @@
+let myTotalMark = 71;
+
+if (myTotalMark >= 80) {
+  console.log("A+");
+} else if (myTotalMark >= 70) {
+  console.log("A");
+} else if (myTotalMark >= 60) {
+  console.log("A-");
+} else if (myTotalMark >= 50) {
+  console.log("B");
+} else if (myTotalMark >= 40) {
+  console.log("C");
+} else if (marks >= 33) {
+  console.log("D");
+} else console.log("F");
---
+++ problem-3.js
@@ -0,0 +1,11 @@
+let num1 = 13;
+let num2 = 79;
+let num3 = 100;
+
+if (num1 > num2 && num1 > num3) {
+  console.log("The largest number is num1 " + num1);
+} else if (num1 < num2 && num2 > num3) {
+  console.log("The largest number is num2 " + num2);
+} else {
+  console.log("The largest number is num3 " + num3);
+}
