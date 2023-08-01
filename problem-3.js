0895720d636ae575aba7745c55055bd1469417d1
Aaban Nayeem <aabannayeem4358@gmail.com>
2023-08-01 13:54:14 +0600

    solve problem 3

--- problem-3.js
+++ problem-3.js
@@ -1,3 +1,5 @@
+//* Problem - 01
+
 let num1 = 13;
 let num2 = 79;
 let num3 = 100;
@@ -8,4 +10,14 @@ if (num1 > num2 && num1 > num3) {
   console.log("The largest number is num2 " + num2);
 } else {
   console.log("The largest number is num3 " + num3);
+}
+
+//* Problem - 02
+
+let side1 = 9;
+let side2 = 8;
+let side3 = 9;
+
+if (side1 == side2 || side1 == side3 || side2 == side3) {
+  console.log("isosceles");
 }
